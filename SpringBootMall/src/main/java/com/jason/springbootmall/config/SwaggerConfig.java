bf8bfdb9cb37f9edf3fc649eb2e7cd7443c9fa91
ShaoweiTeng <sdonline13@gmail.com>
2023-04-29 11:32:40 +0800

    update swagger

--- SpringBootMall/src/main/java/com/jason/springbootmall/config/SwaggerConfig.java
+++ SpringBootMall/src/main/java/com/jason/springbootmall/config/SwaggerConfig.java
@@ -1,8 +1,10 @@
-package config;
+package com.jason.springbootmall.config;
 
 import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -13,10 +15,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
 
-@Configuration
-@EnableSwagger2
-public class SwaggerConfig {
 
+@EnableSwagger2
+@EnableWebMvc
+public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -30,11 +32,11 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo() {
         return new ApiInfo(
-                "SpringBoot Mail 測試 Api",
-                "",
+                "Mall API ",
+                "spring boot  api 測試",
                 "v1.0",
                 "",
-                new Contact("Shao Wei", "",
+                new Contact("Shao Wei Teng", "",
                         "sdonline13@gmail.com"),
                 "", "", Collections.emptyList());
     }
