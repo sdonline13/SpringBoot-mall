8c2ec2e34c5a8beaa7f6a3ca806fd60050d394c9
ShaoweiTeng <sdonline13@gmail.com>
2023-04-29 10:33:55 +0800

    first update

---
+++ SpringBootMall/src/main/java/com/jason/springbootmall/SpringBootMallApplication.java
@@ -0,0 +1,13 @@
+package com.jason.springbootmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMallApplication.class, args);
+    }
+
+}
