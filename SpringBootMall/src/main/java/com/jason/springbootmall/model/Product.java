e7b0c19b0092739a62749f68eaa5c4a2c93a337c
ShaoweiTeng <sdonline13@gmail.com>
2023-04-30 11:04:42 +0800

    修復時間戳 顯示問題

--- SpringBootMall/src/main/java/com/jason/springbootmall/model/Product.java
+++ SpringBootMall/src/main/java/com/jason/springbootmall/model/Product.java
@@ -1,8 +1,9 @@
 package com.jason.springbootmall.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
-import java.util.Date;
+import java.sql.Date;
 
 @Data
 public class Product {
@@ -13,7 +14,9 @@ public class Product {
     private Integer price;
     private Integer stock;
     private String  description;
+   @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date    createdDate;
+   @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date    lastModifiedDate;
 
     public enum  PriductCategory{
